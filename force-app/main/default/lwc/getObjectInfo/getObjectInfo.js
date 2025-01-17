785c9488bc48f7fdc290ef1dd0832425f0a5726f
RoshanChaturpale <roshan.chaturpale1993@gmail.com>
2025-01-18 00:52:51 +0530

    insert

---
+++ force-app/main/default/lwc/getObjectInfo/getObjectInfo.js
@@ -0,0 +1,13 @@
+import { LightningElement, wire } from 'lwc';
+import {getObjectInfo} from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+
+export default class GetObjectInfo extends LightningElement {
+
+    @wire(getObjectInfo , {objectApiName: ACCOUNT_OBJECT})
+    getData(data, error){
+        if(data){
+            console.log('this is my data -==' ,data);
+        }
+    }
+}
