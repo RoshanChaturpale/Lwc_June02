1febeaa435254c575a2554517b8fcb528b020559
RoshanChaturpale <roshan.chaturpale1993@gmail.com>
2025-01-23 02:02:54 +0530

    aa

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class CarCard extends LightningElement {}
+import NAME_FIELD from '@salesforce/schema/Car__c.Name';
+import PUCTURE_URL_FIELD from '@salesforce/schema/Car__c.Picture_URL__c';
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c';
+import MAKE_FILED from '@salesforce/schema/Car__c.Make__c';
+import MSRP_FILED from '@salesforce/schema/Car__c.MSRP__c';
+import FUEL_FIELD from '@salesforce/schema/Car__c.Fuel_Type__c';
+import SEAT_FEILD from '@salesforce/schema/Car__c.Number_of_Seats__c';
+import CONTROL_FILED from '@salesforce/schema/Car__c.Control__c';
+import { getFieldValue} from 'lightning/uiRecordApi';
+
+
+export default class CarCard extends LightningElement {
+
+  categoryField = CATEGORY_FIELD;
+  makeField  = MAKE_FILED;
+  msrpField = MSRP_FILED;
+  fuelField = FUEL_FIELD;
+  seatField = SEAT_FEILD;
+  controlField = CONTROL_FILED;
+
+
+  
+  carName;
+  carPictureUrl;
+
+    @api recordId = 'a1LJ30000008ackMAA';
+
+    handleRecordLoaded(event){
+
+        const {records} = event.detail;
+        const recordData = records[this.recordId];
+        this.carName = getFieldValue(recordData, NAME_FIELD);
+        this.carPictureUrl = getFieldValue(recordData, PUCTURE_URL_FIELD);
+    }
+}
